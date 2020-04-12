8b3b79d51bff19a36cfca38ecc1ee50a7c06377e
Lord1Luciani <17690123+Lord1Luciani@users.noreply.github.com>
2020-04-12 03:12:35 +0200

    fixes, link, space, quoted_status

--- handlers/handleSendMessage.js
+++ handlers/handleSendMessage.js
@@ -9,8 +9,10 @@ const handleSendMessage = async (bot, tweet, groups, settings) => {
     if (message.trash) {
       return
     }
+    console.time(i)
 
     setTimeout(() => {
+      console.timeEnd(i)
       try {
         if (message.method === 'sendMessage') {
           bot.telegram[message.method](group, message.text, {
@@ -28,7 +30,7 @@ const handleSendMessage = async (bot, tweet, groups, settings) => {
         console.log(error)
       }
       message = null
-    }, i * 1000)
+    }, i * 1500)
   })
 }
 
@@ -89,6 +91,10 @@ class Message {
   }
 
   linkMyself () {
+    return `https://twitter.com/${this.tweet.user.screen_name}`
+  }
+
+  linkToCurrentPost () {
     return `https://twitter.com/${this.tweet.user.screen_name}/status/${this.tweet.id_str}`
   }
 
@@ -111,27 +117,28 @@ class Message {
 
     let text = []
     const textTw = this.deleteLinks(this.tweet.full_text)
-    const textQuo = this.deleteLinks(this.tweet.is_quote_status ? this.tweet.quoted_status.full_text : '')
+    const textQuo = this.deleteLinks(this.tweet.quoted_status ? this.tweet.quoted_status.full_text : '') // this.tweet.is_quote_status &&
     const textRt = this.deleteLinks(this.tweet.retweeted_status ? this.tweet.retweeted_status.full_text : '')
 
     text.push(`${this.reply ? `#reply\n<code>${this.reply.name}:</code>\n${this.reply.full_text}\n↓\n` : ''}`)
 
     // const reply = this.reply ? '' : '\n'
-    const linkToPost = this.linkMyself()
+    const linkToPost = this.linkToCurrentPost()
+    const linkMyself = this.linkMyself()
 
     if (this.tweet.quoted_status) {
-      text.push(`${this.settings.name ? ` <code>${this.tweet.user.name}:</code> ` : ''}`)
+      text.push(`${this.settings.name ? ` <a href="${linkMyself}">${this.tweet.user.name}:</a> ` : ''}`)
       text.push(`${textTw ? `\n${textTw}\n↓\n` : ''}`) // ${reply}
-      text.push(`${textQuo ? `<code>${this.tweet.quoted_status.user.name}:</code>\n<i>${textQuo}</i>\n\n` : ''}`)
+      text.push(`${textQuo ? `<a href="${linkMyself}">${this.tweet.quoted_status.user.name}:</a>\n<i>${textQuo}</i>\n\n` : ''}`)
       text.push(`${this.settings.link ? `<a href="${linkToPost}">Twitter</a>` : ''}`)
     } else if (this.tweet.retweeted_status) {
-      text.push(`${this.settings.name ? `<code>${this.tweet.user.name}</code>` + ' ' : ''}`)
+      text.push(`${this.settings.name ? `<a href="${linkMyself}">${this.tweet.user.name}</a>` + ' ' : ''}`)
       text.push('#retweet ')
-      text.push(`${this.settings.from ? `<code>from ${this.tweet.retweeted_status.user.name}</code>` : ''}`)
+      text.push(`${this.settings.from ? `from <a href="${linkMyself}">${this.tweet.retweeted_status.user.name}</a>` : ''}`)
       text.push(`${textRt ? '\n' + textRt + '\n\n' : ''}`) // ${reply}
       text.push(`${this.settings.link ? `<a href="${linkToPost}">Twitter</a>` : ''}`)
     } else {
-      text.push(`${(this.settings.name || this.reply) ? `<code>${this.tweet.user.name}:</code> ` : ''}`)
+      text.push(`${(this.settings.name || this.reply) ? `<a href="${linkMyself}">${this.tweet.user.name}:</a> ` : ''}`)
       text.push(`${textTw ? `\n${textTw}\n\n` : '🖼\n'}`) // ${reply}
       text.push(`${this.settings.link ? `<a href="${linkToPost}">Twitter</a>` : ''}`)
     }
@@ -139,11 +146,16 @@ class Message {
     text = text.join('').trim()
     this.preview = !(text.indexOf('https://t.co') > -1) // ???
 
+    if (!this.preview) {
+      const link = text.match(/https:\/\/t.co\/\S*/m)[0]
+      text = `<a href="${link}">&#8288;</a>`.concat(text)
+    }
+
     return text
   }
 
   getMedia () {
-    let extendedEntities = this.tweet.is_quote_status ? this.tweet.quoted_status.extended_entities : this.tweet.extended_entities
+    let extendedEntities = this.tweet.quoted_status ? this.tweet.quoted_status.extended_entities : this.tweet.extended_entities
     extendedEntities = this.tweet.retweeted_status ? this.tweet.retweeted_status.extended_entities : extendedEntities
     extendedEntities = this.tweet.extended_entities ? this.tweet.extended_entities : extendedEntities
     let medias = ''
